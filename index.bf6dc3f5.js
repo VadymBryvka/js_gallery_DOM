92599a303e11464c6fc2b246263e8d6ca0fe0362
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-07-09 20:59:47 -0500

    make build

---
+++ index.bf6dc3f5.js
@@ -0,0 +1,2 @@
+document.querySelector("ul").addEventListener("click",function(e){e.preventDefault(),document.getElementById("largeImg").setAttribute("src","".concat(e.target.closest(".list-item__link").href))});
+//# sourceMappingURL=index.bf6dc3f5.js.map
